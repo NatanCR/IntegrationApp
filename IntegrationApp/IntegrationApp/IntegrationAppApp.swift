b815b2a5372d55675bdc7e5fc3295a6d6fef3233
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-10-26 21:33:24 -0300

    initial commit

---
+++ IntegrationApp/IntegrationApp/IntegrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationAppApp.swift
+//  IntegrationApp
+//
+//  Created by Natan de Camargo Rodrigues on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
