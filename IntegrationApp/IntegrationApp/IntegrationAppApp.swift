02e4e0815072c1b3b356d568cbb1555554eaaaa0
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-10-28 13:23:19 -0300

    create xcode project

---
+++ IntegrationApp/IntegrationApp/IntegrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationAppApp.swift
+//  IntegrationApp
+//
+//  Created by Natan de Camargo Rodrigues on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
