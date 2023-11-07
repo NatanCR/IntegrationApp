c3ebcccef02fb782514c4bd3bea8e033398fa038
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-11-07 14:25:50 -0300

    ButtonInital

--- ScreenEvents.swift
+++ ScreenEvents.swift
@@ -1,10 +1,3 @@
-//
-//  SheetCreatEvent.swift
-//  IntegrationApp
-//
-//  Created by Guilherme Borges on 06/11/23.
-//
-
 import SwiftUI
 
 struct ScreenEvents: View {
@@ -16,13 +9,18 @@ struct ScreenEvents: View {
         Button(action: {
             isSheetPresented.toggle()
         }) {
-            Text("Abrir Sheet")
+            Circle()
+                .foregroundColor(Color.orange)
+                .frame(width: 45, height: 50)
+                .overlay(
+                    Text("+")
+                        .font(.system(size: 35, weight: .thin))
+                        .foregroundColor(.white)
+                        .position(x: 22.5, y: 22.5)
+                )
         }
         .sheet(isPresented: $isSheetPresented, content: {
             SheetCreateEvent(title: $title, selectedDate: $selectedDate, isSheetPresented: $isSheetPresented)
-//            SheetComponent(isSheetPresented: $isSheetPresented)
         })
     }
 }
-
-
