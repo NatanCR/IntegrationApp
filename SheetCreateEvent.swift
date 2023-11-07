71031f68e070557c729a4b1412c46bb05c7ee909
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-11-07 15:06:39 -0300

    Iconesadd

--- SheetCreateEvent.swift
+++ SheetCreateEvent.swift
@@ -12,34 +12,40 @@ struct SheetCreateEvent: View {
     @Binding var selectedDate: Date
     @Binding var isSheetPresented: Bool
     @State private var icons: [String] = ["icon1", "icon2", "icon3"]
-    
+
+   // número de colunas de LazyVGrid
+    let gridColumns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
         NavigationView {
             Form {
-                Section(header: TitleComponent(title: "Título").bold().foregroundColor(.black)) {
-                    TextFieldComponent(text: $title, placeholder: "Digite o nome do evento...")
+                Section(header: Text("Título").bold().foregroundColor(.black)) {
+                    TextField("Digite o nome do evento...", text: $title)
                 }
-                Section(header: TitleComponent(title: "Prazo Final").bold().foregroundColor(.black)) {
+                Section(header: Text("Prazo Final").bold().foregroundColor(.black)) {
                     HStack {
                         DatePickerComponent(selectedDate: $selectedDate)
-                        Spacer(minLength:220)
+                        Spacer(minLength: 220)
                     }
-                    
                 }
-                Section(header: TitleComponent(title: "ícone").bold().foregroundColor(.black)) {
-                    HStack(spacing:10) {
+                Section(header: Text("Ícone").bold().foregroundColor(.black)) {
+                    LazyVGrid(columns: gridColumns, spacing: 10) {
                         ForEach(icons, id: \.self) { iconName in
                             IconComponent(imageName: "img1")
                         }
                         AddIconComponent(addIconAction: {
-                
                             icons.append("newIcon")
                         })
                     }
                 }
                 .listRowBackground(Color.clear)
             }
-            .navigationTitle("Criar Evento")
+            .navigationBarTitle("Criar Evento", displayMode: .inline)
             .navigationBarItems(
                 leading: Button("Cancelar") {
                     isSheetPresented = false
