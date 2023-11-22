4d29743839aac8bea87c2ee92c0a41dfaa04ba35
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-11-22 14:16:53 -0300

    FIX: dependecias

--- IntegrationApp/Views/Components/SurveyComponent.swift
+++ IntegrationApp/Views/Components/SurveyComponent.swift
@@ -9,27 +9,37 @@ import SwiftUI
 
 struct SurveyComponent: View {
     var options: [PollOption]
+    @Environment (\.screenSize) var screenSize
 
     var body: some View {
-        //organiza a exibição das opções da enquete
-        VStack(alignment: .leading, spacing: 10) {
-            // Loop através das opções da enquete
-            ForEach(options) { option in
-                // organiza a exibição do nome da opção e a barra de progresso
-                VStack {
-                    // exibie o nome da opção e a quantidade de votos
-                    HStack {
-                        Text(option.name)
-                        Spacer()
-                        Text("\(option.votes)")
+        // Adiciona um quadrado em volta do componente da enquete
+        RoundedRectangle(cornerRadius: 10) // ajuste o valor do raio conforme necessário
+            .stroke(Color.black, lineWidth: 1) // ajuste a cor e a largura da linha conforme necessário
+            .padding(10) // ajuste o espaçamento interno conforme necessário
+            .overlay(
+                // Organiza a exibição das opções da enquete
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    // Loop através das opções da enquete
+                    ForEach(options) { option in
+                        // Organiza a exibição do nome da opção e a barra de progresso
+                        VStack {
+                            
+                            // Exibe o nome da opção e a quantidade de votos
+                            HStack {
+                                Text(option.name)
+                                Spacer()
+                                Text("\(option.votes)")
+                            }
+                            // Componente da barra de progresso com base na porcentagem de votos em relação ao total
+                            ProgressBarComponent(value: Double(option.votes) / Double(totalVotes()))
+                                .frame(height: 5)
+                        }
                     }
-                    // Componente da barra de progresso com base na porcentagem de votos em relação ao total
-                    ProgressBarComponent(value: Double(option.votes) / Double(totalVotes()))
-                        .frame(height: 10)
                 }
-            }
-        }
-        .padding()
+                .padding()
+            )
+            .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.5)
     }
 
     // Método para calcular o total de votos em todas as opções da enquete
@@ -37,8 +47,6 @@ struct SurveyComponent: View {
         return options.reduce(0) { $0 + $1.votes }
     }
 }
-
-
 
 //
 //#Preview {
