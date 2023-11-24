93d8d909fce758237ecfcbca653aa29106cc1511
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-11-24 17:41:32 -0300

    fix: corrigindo sheet finance

--- IntegrationApp/Views/SheetsViews/SheetCreateFinance.swift
+++ IntegrationApp/Views/SheetsViews/SheetCreateFinance.swift
@@ -8,48 +8,79 @@
 import SwiftUI
 
 struct SheetCreateFinance: View {
+    @State var inputTitle: String = ""
+    @State var selectedDate = Date()
     @State var inputTotalValue: Double = 0.0
     @State var valuePerMemberCalculated: Double = 0.0
     @Environment (\.screenSize) var screenSize
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         NavigationStack {
-                SheetCreateEvent(placeholderText: "Digite o nome do débito...", sheetBarTitle: "Criar Débito", isFinanceSheetView: true)
-            
-            VStack(alignment: .leading) {
-                Section {
-                    NumberFieldComponent(totalValue: $inputTotalValue)
-                        .onChange(of: inputTotalValue) { newValue in
-                            //chama a função para atualizar o valor por membro
+            VStack {
+                Color.primaryBlue
+                    .frame(height: 50)
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .leading){
+                    Section {
+                        TextFieldComponent(valueText: $inputTitle, placeholder: "Digite o nome do débito...")
+                    } header: {
+                        Text("Título").bold().foregroundStyle(Color.black)
+                    }
+                    
+                    Section {
+                        DatePickerComponent(selectedDate: $selectedDate)
+                    } header: {
+                        Text("Data do Evento").bold().foregroundStyle(Color.black)
+                    }
+                    
+                    Section {
+                        NumberFieldComponent(totalValue: $inputTotalValue)
+                            .onChange(of: inputTotalValue) { newValue in
+                                //chama a função para atualizar o valor por membro
+                            }
+                    } header: {
+                        Text("Valor Total").bold().foregroundStyle(Color.black)
+                    }
+                    
+                    Section {
+                        VStack(alignment: .leading) {
+                            if valuePerMemberCalculated == 0.0 {
+                                Text("R$ 00,00")
+                                    .padding(.horizontal, 8)
+                                    .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.07, alignment: .leading)
+                                    .background(Color.primaryBlue)
+                                    .cornerRadius(15)
+                                    .foregroundColor(Color("TextFieldColor"))
+                            } else {
+                                Text("\(valuePerMemberCalculated)")
+                                    .padding(.horizontal, 8)
+                                    .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.07)
+                                    .background(Color.primaryBlue)
+                                    .cornerRadius(15)
+                                    .foregroundColor(Color("TextFieldColor"))
+                            }
+                            
                         }
-                } header: {
-                    Text("Valor Total").bold().foregroundStyle(Color.black)
+                    } header: {
+                        Text("Valor por Membro").bold().foregroundStyle(Color.black)
+                    }
+                    
                 }
                 
-                Section {
-                    VStack(alignment: .leading) {
-                        if valuePerMemberCalculated == 0.0 {
-                            Text("R$ 00,00")
-                                .padding(.horizontal, 8)
-                                .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.07, alignment: .leading)
-                                .background(Color.primaryBlue)
-                                .cornerRadius(15)
-                                .foregroundColor(Color("TextFieldColor"))
-                        } else {
-                            Text("\(valuePerMemberCalculated)")
-                                .padding(.horizontal, 8)
-                                .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.07)
-                                .background(Color.primaryBlue)
-                                .cornerRadius(15)
-                                .foregroundColor(Color("TextFieldColor"))
-                        }
-                        
-                    }
-                } header: {
-                    Text("Valor por Membro").bold().foregroundStyle(Color.black)
-                }
             }
             Spacer()
+            .navigationBarTitle("Criar Débito", displayMode: .inline)
+            .navigationBarItems(
+                leading: Button("Cancelar") {
+                    dismiss()
+                },
+                trailing: Button("Criar") {
+                    //chamar função de salvar novo evento
+                    dismiss()
+                }.bold()
+            )
         }
     }
 }
