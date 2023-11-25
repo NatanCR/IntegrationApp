35d6b1d9d0699e49caeac19c98f2dfab617269c8
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-11-25 01:05:27 -0300

    Segment

--- IntegrationApp/Views/CreateSurveyView.swift
+++ IntegrationApp/Views/CreateSurveyView.swift
@@ -13,12 +13,15 @@ struct CreateSurveyView: View {
     @Binding var title: String
     @State private var showingResults = false
     @State private var newOption = ""
+    @State private var selectedSegment1 = 0
+    @State private var selectedSegment2 = 0
     @State private var options: [SurveyOption] = [
         SurveyOption(id: 1, name: "Vermelho", votes: 0),
         SurveyOption(id: 2, name: "Azul", votes: 0),
         SurveyOption(id: 3, name: "Verde", votes: 0)
     ]
-
+    @Environment (\.screenSize) var screenSize
+    
     var body: some View {
         
         VStack(alignment: .leading, spacing: 10) {
@@ -27,7 +30,7 @@ struct CreateSurveyView: View {
                 options: $options,
                 isVotingEnabled: true
             )
-
+            
             TextField("Toque para adicionar", text: $newOption, onCommit: {
                 if !newOption.isEmpty {
                     let newId = options.count + 1
@@ -37,28 +40,60 @@ struct CreateSurveyView: View {
             })
             .textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
-
+            
             Button("Show Results") {
                 showingResults.toggle()
             }
             .sheet(isPresented: $showingResults) {
-//                VStack{
-//                    TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
-//                        .foregroundColor(Color("TextFieldColor"))
-//                }
-                Form {
-                    Section(header: Text("Título").bold().foregroundColor(.black)){
-                        TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
-                            .foregroundColor(Color("TextFieldColor"))
-                    }.listRowBackground(Color.clear)
+                NavigationStack {
+                    VStack(alignment: .leading) {
+                        Section(header: TitleComponent(title: "Título")) {
+                            TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
+                                .foregroundColor(Color("TextFieldColor"))
+                        }.listRowBackground(Color.clear)
+                        Section(header: TitleComponent(title: "Prazo Final")) {
+                            TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
+                                .foregroundColor(Color("TextFieldColor"))
+                        }.listRowBackground(Color.clear)
+                    }
+                    VStack(alignment: .leading, spacing: 15){
+                        Section(header: TitleComponent(title: "Tipo de Enquete")){
+                            Picker("Segmento", selection: $selectedSegment1) {
+                                Text("Opção 1").tag(0)
+                                Text("Opção 2").tag(1)
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                            .frame(width: screenSize.width * 0.9)
+                            
+                            TitleComponent(title: "Tipo de Escolha")
+                            Picker("Segmento", selection: $selectedSegment2) {
+                                Text("Opção 1").tag(0)
+                                Text("Opção 2").tag(1)
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                            .frame(width: screenSize.width * 0.9)
+                        }
+                    }
                     
+                    VStack(alignment: .leading){
+                        Section(header: TitleComponent(title: "Opções")){
+                            
+                        }
+                    }
+
+                    Spacer()
+                        .navigationBarTitle("Criar Enquete", displayMode: .inline)
+                        .navigationBarItems(
+                            leading: Button("Cancelar") {
+                                showingResults = false
+                            },
+                            trailing: Button("Criar") {
+                                showingResults = false
+                            }
+                        )
                 }
-//                SurveyComponent(
-//                    question: "Qual é a sua cor favorita?",
-//                    options: $options,
-//                    isVotingEnabled: false
-//                )
             }
+            
         }
         .padding()
     }
@@ -66,7 +101,30 @@ struct CreateSurveyView: View {
 
 
 
-//// Definição da classe para representar uma enquete observável
+//    .sheet(isPresented: $showingResults) {
+//        NavigationStack{
+//            VStack(alignment: .leading) {
+//                Section(header: Text("Título").bold().foregroundColor(.black)){
+//                    TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
+//                        .foregroundColor(Color("TextFieldColor"))
+//                }.listRowBackground(Color.clear)
+//                Section(header: Text("Título").bold().foregroundColor(.black)){
+//                    TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
+//                        .foregroundColor(Color("TextFieldColor"))
+//                }.listRowBackground(Color.clear)
+//                Spacer()
+//            }
+//
+//            //                SurveyComponent(
+//            //                    question: "Qual é a sua cor favorita?",
+//            //                    options: $options,
+//            //                    isVotingEnabled: false
+//            //                )
+//        }
+//
+//
+//    }
+////// Definição da classe para representar uma enquete observável
 //class Poll: ObservableObject {
 //    @Published var question: String
 //    @Published var options: [SurveyOption]
