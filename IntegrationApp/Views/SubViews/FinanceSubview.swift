3c2712daab029b07686f40cc8ff6677fe792e244
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-11-27 16:36:10 -0300

    feat: posicionando wallet

--- IntegrationApp/Views/SubViews/FinanceSubview.swift
+++ IntegrationApp/Views/SubViews/FinanceSubview.swift
@@ -9,15 +9,24 @@ import SwiftUI
 
 struct FinanceSubview: View {
     @Environment (\.screenSize) var screenSize
+    @State var currentWalletValue: Double = 0.00
     
     var body: some View {
-        VStack {
+        ZStack {
             CreateEventButton(view: SheetCreateFinance())
                 .position(CGPoint(x: screenSize.width * 0.88, y: screenSize.height * 0.78))
+            
+            WalletCardComponent(walletValue: currentWalletValue)
+                .position(CGPoint(x: screenSize.width * 0.2, y: screenSize.height * 0.78))
+        }
+        .onAppear{
+            //TODO: Função para: - carregar valor do caixa
+            //                   - carregar financeiro em aberto
+            
         }
     }
 }
 
 #Preview {
-    FinanceSubview()
+    FinanceSubview(currentWalletValue: 0.00)
 }
