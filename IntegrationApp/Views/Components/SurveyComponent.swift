a7403c7b258c57533b6b85b7fd85b045b97e9bd9
Guilborges <89112414+Guilborges@users.noreply.github.com>
2023-11-27 19:39:48 -0300

    Lista arredondada

--- IntegrationApp/Views/Components/SurveyComponent.swift
+++ IntegrationApp/Views/Components/SurveyComponent.swift
@@ -7,70 +7,85 @@
 
 import SwiftUI
 
-// Componente reutilizável para exibir uma enquete
+
 struct SurveyComponent: View {
-//    var question: String
+    // var question: String
     @State private var newOption = ""
     @Binding var options: [SurveyOption]
     var isVotingEnabled: Bool
     @State private var selectedOptionId: Int?
-
+    
     @Environment(\.screenSize) var screenSize
-
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-//            Text(question)
-//                .font(.headline)
-
-            ForEach(options.indices, id: \.self) { index in
-                HStack {
-                    // Bolinha que muda de cor ao votar
-                    Circle()
-                        .stroke(Color.gray, lineWidth: 1)
-                        .background(Circle().fill(selectedOptionId == options[index].id ? Color.blue : Color.clear))
-                        .frame(width: 20, height: 20)
-                        .onTapGesture {
-                            if isVotingEnabled && selectedOptionId == nil {
-                                selectedOptionId = options[index].id
-                                options[index].votes += 1
-                            }
-                        }
-
-                    Text(options[index].name)
-                    Spacer()
-                    Text("\(options[index].votes)")
-                }
-                Divider()
-//                ProgressBarComponent(value: Double(options[index].votes) / Double(totalVotes()))
-//                    .frame(height: 5)
-                
-            }
-            TextField("Toque para adicionar", text: $newOption, onCommit: {
-                // Verifica se o texto não está vazio
-                if !newOption.isEmpty {
-                    // Cria um novo ID baseado na contagem de opções existentes
-                    let newId = options.count + 1
-
-                    // Adiciona a nova opção à lista de opções
-                    options.append(SurveyOption(id: newId, name: newOption, votes: 0))
-
-                    // Limpa o campo de texto
-                    newOption = ""
-                }
-            })
-            
-        }
-        .padding()
-        .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.5)
-        .overlay(
+        ZStack {
             RoundedRectangle(cornerRadius: 10)
                 .stroke(Color.black, lineWidth: 1)
-        )
-        .background(Color.primaryBlue)
-        .cornerRadius(10)
-
+                .frame(width: screenSize.width * 0.9)
+                .background(Color.primaryBlue)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    // Text(question)
+                    //     .font(.headline)
+                    
+                    if options.isEmpty {
+                        // Mostra o TextField quando a lista está vazia
+                        TextField("Toque para adicionar", text: $newOption, onCommit: {
+                            if !newOption.isEmpty {
+                                let newId = 1 // Como a lista está vazia, o ID pode começar em 1
+                                options.append(SurveyOption(id: newId, name: newOption, votes: 0))
+                                newOption = ""
+                            }
+                        })
+//                            .background(Color.primaryBlue)
+//                            .cornerRadius(10)
+                    } else {
+                        ForEach(options.indices, id: \.self) { index in
+                            HStack {
+                                // Bolinha que muda de cor ao votar
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 1)
+                                    .background(Circle().fill(selectedOptionId == options[index].id ? Color.blue : Color.clear))
+                                    .frame(width: 20, height: 20)
+                                    .onTapGesture {
+                                        if isVotingEnabled && selectedOptionId == nil {
+                                            selectedOptionId = options[index].id
+                                            options[index].votes += 1
+                                        }
+                                    }
+                                
+                                Text(options[index].name)
+                                Spacer()
+                                Text("\(options[index].votes)")
+                            }
+//                            .background(Color.primaryBlue)
+//                            .cornerRadius(10)
+                            Divider()
+                                .frame(width: screenSize.width * 0.9, height: screenSize.height * 0.001)
+                                .background(Color.white)
+                            // ProgressBarComponent(value: Double(options[index].votes) / Double(totalVotes()))
+                            //     .frame(height: 5)
+                            
+                            // Coloca o TextField após o último item
+                            if index == options.count - 1 {
+                                TextField("Toque para adicionar", text: $newOption, onCommit: {
+                                    if !newOption.isEmpty {
+                                        let newId = options.count + 1
+                                        options.append(SurveyOption(id: newId, name: newOption, votes: 0))
+                                        newOption = ""
+                                    }
+                                })
+                            }
+                        }.cornerRadius(10)
+                    }
+                }
+                .padding()
+            }.cornerRadius(10)
+        }
+        .frame(width: screenSize.width * 0.9)
     }
-
+    
     func totalVotes() -> Int {
         return options.reduce(0) { $0 + $1.votes }
     }
--- IntegrationApp/Views/CreateSurveyView.swift
+++ IntegrationApp/Views/CreateSurveyView.swift
@@ -16,9 +16,9 @@ struct CreateSurveyView: View {
     @State private var selectedSegment1 = 0
     @State private var selectedSegment2 = 0
     @State private var options: [SurveyOption] = [
-        SurveyOption(id: 1, name: "Vermelho", votes: 0)
-//        SurveyOption(id: 2, name: "Azul", votes: 0),
-//        SurveyOption(id: 3, name: "Verde", votes: 0)
+//        SurveyOption(id: 1, name: "Vermelho", votes: 0)
+        //        SurveyOption(id: 2, name: "Azul", votes: 0),
+        //        SurveyOption(id: 3, name: "Verde", votes: 0)
     ]
     @Environment (\.screenSize) var screenSize
     
@@ -26,7 +26,7 @@ struct CreateSurveyView: View {
         
         VStack(alignment: .leading, spacing: 10) {
             SurveyComponent(
-//                question: "Qual é a sua cor favorita?",
+                //                question: "Qual é a sua cor favorita?",
                 options: $options,
                 isVotingEnabled: true
             )
@@ -39,64 +39,58 @@ struct CreateSurveyView: View {
             .sheet(isPresented: $showingResults) {
                 NavigationStack {
                     ScrollView{
-                    VStack(alignment: .leading) {
-                        
-                        Section(header: TitleComponent(title: "Título")) {
-                            TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
-                                .foregroundColor(Color("TextFieldColor"))
-                        }.listRowBackground(Color.clear)
-                        
-                        Section(header: TitleComponent(title: "Prazo Final")) {
-                            TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
-                                .foregroundColor(Color("TextFieldColor"))
-                        }.listRowBackground(Color.clear)
-                    }
-                        
-                    VStack(alignment: .leading, spacing: 15){
-                        Section(header: TitleComponent(title: "Tipo de Enquete")){
-                            Picker("Segmento", selection: $selectedSegment1) {
-                                Text("Atividades").tag(0)
-                                Text("Financeiro").tag(1)
-                            }
-                            .pickerStyle(SegmentedPickerStyle())
-                            .frame(width: screenSize.width * 0.9)
+                        VStack(alignment: .leading) {
                             
-                            TitleComponent(title: "Tipo de Escolha")
+                            Section(header: TitleComponent(title: "Título")) {
+                                TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
+                                    .foregroundColor(Color("TextFieldColor"))
+                            }.listRowBackground(Color.clear)
                             
-                            VStack(alignment: . leading){
-                                Picker("Segmento", selection: $selectedSegment2) {
-                                    Text("Única").tag(0)
-                                    Text("Múltipla").tag(1)
+                            Section(header: TitleComponent(title: "Prazo Final")) {
+                                TextFieldComponent(text: $title, placeholder: "Digite o nome da enquete...")
+                                    .foregroundColor(Color("TextFieldColor"))
+                            }.listRowBackground(Color.clear)
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 15){
+                            
+                            Section(header: TitleComponent(title: "Tipo de Enquete")){
+                                Picker("Segmento", selection: $selectedSegment1) {
+                                    Text("Atividades").tag(0)
+                                    Text("Financeiro").tag(1)
                                 }
-                                
                                 .pickerStyle(SegmentedPickerStyle())
                                 .frame(width: screenSize.width * 0.9)
+                                
+                                TitleComponent(title: "Tipo de Escolha")
+                                
+                                VStack(alignment: . leading){
+                                    Picker("Segmento", selection: $selectedSegment2) {
+                                        Text("Única").tag(0)
+                                        Text("Múltipla").tag(1)
+                                    }
+                                    
+                                    .pickerStyle(SegmentedPickerStyle())
+                                    .frame(width: screenSize.width * 0.9)
+                                }
                             }
                         }
-                    }
-                    
+                        
                         VStack(){
                             Section(header: TitleComponent(title: "Opções")){
                                 SurveyComponent(
-//                                    question: "Qual é a sua cor favorita?",
+                                    //                                    question: "Qual é a sua cor favorita?",
                                     options: $options,
                                     isVotingEnabled: false
                                     
                                 )
-                
-//                                TextField("Toque para adicionar", text: $newOption, onCommit: {
-//                                    if !newOption.isEmpty {
-//                                        let newId = options.count + 1
-//                                        
-//                                        options.append(SurveyOption(id: newId, name: newOption, votes: 0))
-//                                        newOption = ""
-//                                    }
-//                                })
+                                .cornerRadius(10)
+                                
                             }
                             
                         }
                     }
-
+                    
                     Spacer()
                         .navigationBarTitle("Criar Enquete", displayMode: .inline)
                         .navigationBarItems(
